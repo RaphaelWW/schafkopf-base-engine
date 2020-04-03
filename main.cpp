e33620cd9856e9827b2f7bda0d59191d956e4db7
Raphael Wild <raphael.wild@tum.de>
2020-04-03 16:30:07 +0200

    Fix bug with wrong player index and modify output

--- main.cpp
+++ main.cpp
@@ -10,24 +10,26 @@
 #include "Game/randomplayer.hpp"
 #include "Game/gamecontroller.hpp"
 #include "Game/tcpplayer.cpp"
+#include "Game/consoleplayer.hpp"
 
 int main() {
 	int wins[4] = { 0 };
 	GameController controller;
-	controller.registerPlayer(new TcpPlayer("127.0.0.1", 8080, &controller, 4));
 	for (int i = 0; i < 3; i++) {
 		controller.registerPlayer(new RandomPlayer());
 	}
-	for (int i = 0; i < 1000; i++) {
+	controller.registerPlayer(new TcpPlayer("127.0.0.1", 8080, &controller, 3));
+	//controller.registerPlayer(new ConsolePlayer());
+
+	for (int i = 0; i < 100000; i++) {
 		GameResult result = controller.initGame();
+		std::cout << "\r" << "Games won: ";
 		for (int i = 0; i < 4; i++) {
 			if (result.winner[i]) {
 				wins[i]++;
 			}
+			std::cout << wins[i] << " ";
 		}
-	}
-	for (int i = 0; i < 4; i++) {
-		std::cout << "Player " << i << " has " << wins[i] << " wins." << std::endl;
 	}
 }
 
